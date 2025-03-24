8f8cd464f02da824498c605b0a1b49fdf4d497dc
eratofly <d_okhotnikova@mail.ru>
2025-03-24 10:28:15 +0300

    table поправила stringHelpers

--- src/StringHelpers.cpp
+++ src/StringHelpers.cpp
@@ -25,20 +25,6 @@ std::vector<std::string> Split(const std::string& str, const std::string& separa
     return strs;
 }
 
-inline void ltrim(std::string& s)
-{
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
-        return !std::isspace(ch);
-        }));
-}
-
-inline void rtrim(std::string& s)
-{
-    s.erase(std::find_if(s.rbegin(), s.rend(), [](unsigned char ch) {
-        return !std::isspace(ch);
-        }).base(), s.end());
-}
-
 std::string RemoveSpacesInBeginAndEndOfWord(const std::string& str)
 {
     std::string newStr = str;
--- src/StringHelpers.h
+++ src/StringHelpers.h
@@ -1,11 +1,8 @@
 #pragma once
 #include <vector>
 #include <string>
+#include <algorithm>
 
 std::vector<std::string> Split(const std::string& str, const std::string& separator);
-
-inline void ltrim(std::string& s);
-
-inline void rtrim(std::string& s);
 
 std::string RemoveSpacesInBeginAndEndOfWord(const std::string& str);
